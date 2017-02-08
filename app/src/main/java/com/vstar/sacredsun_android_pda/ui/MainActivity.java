e5726669ea6e19a721b0b79c4d421309fed7f096
tanghuailong <tanghuailong@gmail.com>
2017-02-08 11:20:22 +0800

    feat: 添加程序的主页面

--- app/src/main/java/com/vstar/sacredsun_android_pda/ui/MainActivity.java
+++ app/src/main/java/com/vstar/sacredsun_android_pda/ui/MainActivity.java
@@ -2,22 +2,13 @@ package com.vstar.sacredsun_android_pda.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.path.android.jobqueue.JobManager;
-import com.vstar.sacredsun_android_pda.App;
 import com.vstar.sacredsun_android_pda.R;
-import com.vstar.sacredsun_android_pda.entity.OrderJob;
-import com.vstar.sacredsun_android_pda.entity.UserEvent;
-import com.vstar.sacredsun_android_pda.util.rxjava.RxBus;
-import com.vstar.sacredsun_android_pda.util.rxjava.RxHelper;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,28 +26,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        subscription = RxBus.getDefault().tObservable(UserEvent.class)
-                .compose(RxHelper.io_main())
-                .subscribe(r -> {
-            Toast.makeText(MainActivity.this,"发送成功 "+r.getMsg(),Toast.LENGTH_LONG).show();
-        },e -> {
-            e.printStackTrace();
-        });
-    }
-
-    @OnClick(R.id.testJob)
-    public void testJob(View view) {
-        Log.d(LOG_TAG,"click button request github");
-        jobManager = App.getInstance().getJobManager();
-        jobManager.addJobInBackground(new OrderJob());
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(!subscription.isUnsubscribed()) {
-            subscription.unsubscribe();
-        }
     }
 }
